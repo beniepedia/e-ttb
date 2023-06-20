2eb74a070db910018a81bb54d15199465b26ec06
beniepedia <beniepedia@gmail.com>
2023-06-21 02:40:26 +0700

    tambah fitur check tanda terima untuk customer

---
+++ public/build/assets/TextArea.0027f5eb.js
@@ -0,0 +1,1 @@
+import{r as c,a as f}from"./app.b2060d12.js";function x({name:e,value:r,className:a,placeHolder:o="",required:t,handleChange:s,disable:n}){const u=c.exports.useRef();return f("textarea",{className:"textarea rounded focus:outline-none focus:border-green-600 border-2 shadow w-full "+a,name:e,value:r,ref:u,placeholder:o,required:t,onChange:d=>s(d),disabled:n})}export{x as T};
